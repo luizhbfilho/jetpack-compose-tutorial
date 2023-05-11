fdbe595e12a68d7758c46553132fd6983015b906
luizhbfilho <luizhbfilho@outlook.com>
2023-05-11 11:12:56 -0300

    Created message data class

--- app/src/main/java/com/luizhbfilho/jetpack_compose_tutorial/MainActivity.kt
+++ app/src/main/java/com/luizhbfilho/jetpack_compose_tutorial/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.luizhbfilho.jetpack_compose_tutorial
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.luizhbfilho.jetpack_compose_tutorial.data.Message
 import com.luizhbfilho.jetpack_compose_tutorial.ui.theme.JetpackcomposetutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Message("Android")
+                    MessageCard(Message("Android", "Hello World"))
                 }
             }
         }
@@ -30,17 +33,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Message(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MessageCard(message: Message) {
+    Column {
+        Text(text = message.author)
+        Text(text = message.body)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetpackcomposetutorialTheme {
-        Message("Android")
+        MessageCard(Message("Android", "Hello World"))
     }
 }
---
+++ app/src/main/java/com/luizhbfilho/jetpack_compose_tutorial/data/Message.kt
@@ -0,0 +1,7 @@
+package com.luizhbfilho.jetpack_compose_tutorial.data
+
+data class Message(
+    val author: String,
+    val body: String
+) {
+}
