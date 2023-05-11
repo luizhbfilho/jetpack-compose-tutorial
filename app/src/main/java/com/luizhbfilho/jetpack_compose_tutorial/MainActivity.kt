ed63adbeb2fcbf33f30b2925ebf96816521d7a3a
luizhbfilho <luizhbfilho@outlook.com>
2023-05-11 11:31:42 -0300

    Adjustments in message card

--- app/src/main/java/com/luizhbfilho/jetpack_compose_tutorial/MainActivity.kt
+++ app/src/main/java/com/luizhbfilho/jetpack_compose_tutorial/MainActivity.kt
@@ -4,14 +4,25 @@ package com.luizhbfilho.jetpack_compose_tutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.luizhbfilho.jetpack_compose_tutorial.data.Message
 import com.luizhbfilho.jetpack_compose_tutorial.ui.theme.JetpackcomposetutorialTheme
 
@@ -34,9 +45,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MessageCard(message: Message) {
-    Column {
-        Text(text = message.author)
-        Text(text = message.body)
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = "Profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape))
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(text = message.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = message.body)
+        }
     }
 }
 
